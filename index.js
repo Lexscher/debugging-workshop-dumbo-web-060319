5691893c22e61304ab114edaf40625f75cbd4b14
Alexander Schelchere <ascellie@gmail.com>
2019-07-22 11:21:50 -0400

    done with HELEN

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  const form = document.getElementById('joke-form');
+  const jokeList = document.getElementById('joke-list');
+  let joke = '';
 
-  function fetchJoke(){
+  function fetchJoke() {
     fetch('https://icanhazdadjoke.com/', {
       headers: {
-        "Accept": "application/json"
+        Accept: 'application/json'
       }
     })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+      .then(res => res.json())
+      .then(jokeData => {
+        // debugger;
+        joke = jokeData.joke;
+      });
   }
 
-  form.addEventListener('submit', (event) => {
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    // console.log(event);
+    // When we enter 'helen' in the input field, here's how we get the value
+    const username = document.getElementById('name-input').value;
+    // username = event.target.querySelector('p').querySelector('input').value;
+    // debugger;
+    if (username === '') return;
+    fetchJoke();
+    if (joke === '') return;
+    // debugger;
 
-    if(username === "") return;
-    fetchJoke()
+    const newJokeLi = document.createElement('li');
+    jokeList.appendChild(newJokeLi);
+
     newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
-  })
-})
+    `;
+  });
+});
